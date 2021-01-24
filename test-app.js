43424a9e5ab4a7e4c1a470206ce75d88a3d40767
Mayur Aitavadekar <mayur.aitavadekar@viit.ac.in>
2021-01-24 17:28:31 +0530

    port changed

--- test-app.js
+++ test-app.js
@@ -47,7 +47,7 @@ app.get("/my-app", (req, res) => {
 });
 
 // ======================= PORT DECLARATION =================================================
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // ======================= LISTEN ON PORT ===================================================
 app.listen(port, () => {
